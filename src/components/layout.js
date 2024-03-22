656c76b0a7149e810824f498783231a206a8d513
danieltandem <tandem.danielvargas@gmail.es>
2024-03-22 09:10:41 +0100

    Paso 3: Utilizar plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,12 @@ import {
     navLinkItem,
     navLinkText
 }  from "./layout.module.css"
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Layout = ({ pageTitle, children }) => {
     return (
     <div className= {container}>
+        <StaticImage src='../images/icon.png' width={100} />
         <nav>
             <ul className= {navLinks}>
                 <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout.js'
+import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
 return (
   <Layout pageTitle='Pagina de Inicio'>
     <p>Robar mola</p>
+    <StaticImage src="https://www.caracolviajero.com.es/blog/wp-content/uploads/2020/05/palacio-real-aranjuez.jpg" alt='exterior del palacio real en Aranjuez'/>
+    <StaticImage src='../images/estanquechinescos.jpg' alt='estanque chinescos'/>
   </Layout>
+
 )
 }
 
@@ -17,3 +21,4 @@ export const Head = () => <title>Home Page</title>
 // Step 3: Export your component
 export default IndexPage
 
+  
